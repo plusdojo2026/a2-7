eb7504ae732a1e86b170d2fcae76cff697cf79a3
plusdojo2026-fukaya <fukaya-asuha-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-14 17:02:11 +0900

    自動でテーブル更新できるように変更　ゴミのレポジトリ作成

--- A2/src/main/java/com/example/demo/repository/GarbageRepository.java
+++ A2/src/main/java/com/example/demo/repository/GarbageRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GarbageRepository extends JpaRepository<T, ID> {
+import com.example.demo.entity.Garbage;
+
+public interface GarbageRepository extends JpaRepository<Garbage, Integer> {
 
 }
