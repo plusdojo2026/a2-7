84712e7caa3ac611002fcf87901de2609fc4d204
plusdojo2026-toyodaakari <toyoda-akari-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-14 16:39:06 +0900

    Shopping

--- A2/src/main/java/com/example/demo/controller/api/ShoppingController.java
+++ A2/src/main/java/com/example/demo/controller/api/ShoppingController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller.api;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ShoppingController {
 
 }
--- A2/src/main/java/com/example/demo/entity/Shopping.java
+++ A2/src/main/java/com/example/demo/entity/Shopping.java
@@ -1,5 +1,27 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="Shopping")
 public class Shopping {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer shopping_id;
+	private String shopping_name;
+	private Integer is_bought;
+	private Integer bought_day;
+	private Integer user_id;
 
 }
