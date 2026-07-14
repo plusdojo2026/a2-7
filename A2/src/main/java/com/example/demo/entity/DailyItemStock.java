3b0d2ccfd27828c93cf1a67b674e006cedffd869
plusdojo2026-yamaguchi <yamaguchi-yu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-14 17:33:04 +0900

    在庫（日用品）のエンティティ

--- A2/src/main/java/com/example/demo/entity/DailyItemStock.java
+++ A2/src/main/java/com/example/demo/entity/DailyItemStock.java
@@ -1,5 +1,31 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity              // データベースの1行と対になっていることを示す。
+@AllArgsConstructor  // 全ての項目を引数として持つコンストラクタを自動定義する。
+@NoArgsConstructor   // 引数なしのコンストラクタ自動で追加
+@Data                // getter、setter、toStringなどの基本的メソッドを自動定義する。
+@Table(name = "Daily_Item_Stock") // マッピングされるテーブルを指定する。
 public class DailyItemStock {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer dailyItemStockId;
+	private String dailyItemStockName;
+	private String category;
+	private LocalDate guideExDate;
+	private LocalDate addDate;
+	private Boolean status;
+	private User user;
+	private DailyItemMaster dailyItemMaster;
 }
