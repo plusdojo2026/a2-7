6d069e31e2f8382f4ba47615d99be5d710936407
plusdojo2026-fukaya <fukaya-asuha-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-15 16:55:41 +0900

    ユーザーレポジトリ更新　ホームレポジトリ削除

--- A2/src/main/java/com/example/demo/repository/UserRepository.java
+++ A2/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<T, ID> {
+import com.example.demo.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
