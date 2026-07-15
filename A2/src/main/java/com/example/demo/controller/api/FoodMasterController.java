82dfbbe2aa84040ab1df549070cb51053e2e6dac
plusdojo2026-yamaguchi <yamaguchi-yu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-15 10:56:43 +0900

    食材マスターのController

--- A2/src/main/java/com/example/demo/controller/api/FoodMasterController.java
+++ A2/src/main/java/com/example/demo/controller/api/FoodMasterController.java
@@ -1,5 +1,24 @@
 package com.example.demo.controller.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.FoodMaster;
+import com.example.demo.repository.FoodMasterRepository;
+
+@RestController
+@CrossOrigin
 public class FoodMasterController {
 
+	@Autowired
+	FoodMasterRepository repository;
+	
+	@GetMapping("/api/food-master")
+	public List<FoodMaster> findAll() {
+		return repository.findAll();
+    }
 }
