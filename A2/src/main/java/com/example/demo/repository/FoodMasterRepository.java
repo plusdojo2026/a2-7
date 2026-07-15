dca8efdbe7a65b99c14c57fca16c2ceeb81d4ddc
plusdojo2026-yamaguchi <yamaguchi-yu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-15 09:56:28 +0900

    食材マスターのリポジトリー

--- A2/src/main/java/com/example/demo/repository/FoodMasterRepository.java
+++ A2/src/main/java/com/example/demo/repository/FoodMasterRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
 
-public class FoodMasterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.FoodMaster;
+
+public interface FoodMasterRepository extends JpaRepository<FoodMaster, Integer> {
 
 }
