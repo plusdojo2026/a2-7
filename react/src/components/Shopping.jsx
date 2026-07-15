84135131731027e2bb0c78e200513fca7df067c3
plusdojo2026-toyodaakari <toyoda-akari-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-15 14:25:41 +0900

    shopping jsx

--- react/src/components/Shopping.jsx
+++ react/src/components/Shopping.jsx
@@ -1,3 +1,18 @@
 function shopping() {
-    
-}
+    return(
+    <>
+        <div className="header">
+            <button className="header_text">作成</button>
+            <button className="header_text">一覧</button>
+        </div>
+
+        <div className="shopping_list">
+
+            <button id="plus">img</button>
+        </div>
+        <button className="mainbutton">作成</button>
+
+        <footer className="footer">アイコンimgを並べる</footer>
+    </>
+    );
+};
