5bd452623b815d5eb22e15f0c0b91dbe0177cdd6
plusdojo2026-toyodaakari <toyoda-akari-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-16 10:25:38 +0900

    Shoppingコントローラー完成

--- A2/src/main/java/com/example/demo/controller/api/ShoppingController.java
+++ A2/src/main/java/com/example/demo/controller/api/ShoppingController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller.api;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,11 +57,56 @@ public class ShoppingController {
 		shoppingItemRepository.saveAll(items);
 	
 	}
-
+	
+	//商品の購入状況を更新
+	@PutMapping("/item/{id}")
+	public void updateShoppingItem(
+			@PathVariable Integer id,
+			@RequestBody ShoppingItem item) {
+		
+		ShoppingItem shoppingItem =
+				shoppingItemRepository.findById(id).orElse(null);
+		
+		if(shoppingItem != null) {
+			shoppingItem.setIsBought(item.getIsBought());
+			shoppingItemRepository.save(shoppingItem);
+		}
+	}
+	
+	//最新リストの未購入商品を取得
+	//latest==訳：最新の
+	@GetMapping("/latest")
+	public List<ShoppingItem> getLatestShoppingItems() {
+		
+		ShoppingList latest =
+				shoppingListRepository.findTopByOrderByCreateDateDesc();
+		
+		if (latest == null) {
+			return new ArrayList<>();
+		}
+		
+		List<ShoppingItem> items = 
+				shoppingItemRepository.findByShoppingListId(
+						latest.getShoppingListid());
+		
+		List<ShoppingItem> result = new ArrayList<>();
+		
+		for (ShoppingItem item : items) {
+			
+			if (item.getIsBought() == 0) {
+				result.add(item);
+			}
+		}
+		
+		return result;
+	}
+	
+	//モーダルに表示する商品一覧を取得
+	@GetMapping("/item/{shoppingListId}")
+	public List<ShoppingItem> getShoppingItems(
+			@PathVariable Integer shoppingListId) {
+		
+		return shoppingItemRepository.findByShoppingListId(shoppingListId);
+	}
 }
 
-
-
-//最新リストの未購入商品を取得
-//新しい買い物リストを保存
-//商品の購入状況を更新
