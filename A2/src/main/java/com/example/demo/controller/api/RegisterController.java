b9031c41b5eb4bc6b061d387105ca2f942ee3d5a
plusdojo2026-fukaya <fukaya-asuha-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-21 14:48:44 +0900

    ユーザー関連修正

--- A2/src/main/java/com/example/demo/controller/api/RegisterController.java
+++ A2/src/main/java/com/example/demo/controller/api/RegisterController.java
@@ -19,16 +19,16 @@ public class RegisterController {
 	@PostMapping
 	public boolean register(@RequestBody User user) {
 
-		if (user.getUser_name() == null || user.getUser_name().isEmpty() || user.getPassword() == null
+		if (user.getUserName() == null || user.getUserName().isEmpty() || user.getPassword() == null
 				|| user.getPassword().isEmpty()) {
 
 			return false;
 		}
 
-//		if (userRepository.existsByUser_name(user.getUser_name())) {
-//
-//			return false;
-//		}
+		if (userRepository.existsByUserName(user.getUserName())) {
+
+			return false;
+		}
 
 		user.setPoint(0);
 
