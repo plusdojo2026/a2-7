9fcaad4f1dd071985c83b7e664820f6d8408d11c
plusdojo2026-fukaya <fukaya-asuha-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-22 16:04:01 +0900

    食事　セッション連携

--- A2/src/main/java/com/example/demo/controller/api/MealController.java
+++ A2/src/main/java/com/example/demo/controller/api/MealController.java
@@ -16,150 +16,148 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entity.Meal;
+import com.example.demo.entity.User;
 import com.example.demo.repository.MealRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class MealController {
-	
+
 	@Autowired
 	private MealRepository repository;
-	
-	//食事内容の登録
+
+	// 食事内容の登録
 	@PostMapping("/api/meal/regist/")
-	private Meal regist(
-			@RequestParam("image") MultipartFile image,
-			@ModelAttribute Meal meal) { //JSONデータをオブジェクトに変換
-		
+	private Meal regist(@RequestParam("image") MultipartFile image, @ModelAttribute Meal meal, HttpSession session) { // JSONデータをオブジェクトに変換
+
 		String fileName;
-		
+
 		try {
-			fileName = saveImage(image); //以下の画像保存メソッドに移動
-			meal.setMealImage(fileName);  //entityに値をセット
-		
-			Integer userId = 1;  //ログイン中のユーザーのｉｄ取得→１は仮置き
-			meal.setUserId(userId);  //mealオブジェクトのuserIdに値をセット
-			repository.save(meal);  //DB保存
+			fileName = saveImage(image); // 以下の画像保存メソッドに移動
+			meal.setMealImage(fileName); // entityに値をセット
+
+			User loginUser = (User) session.getAttribute("loginUser");
+			if (loginUser == null) {
+			    throw new RuntimeException("ログインしてください");
+			}
+			Integer userId = loginUser.getUserId();
+
+			meal.setUserId(userId);
+			repository.save(meal); // DB保存
 			return meal;
-		
+
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	//登録された画像ファイルをuploadsフォルダに保存
-	private String saveImage(MultipartFile file) 
-		throws IOException{
-		String fileName =
-				System.currentTimeMillis() + "_" + file.getOriginalFilename(); //現在時刻＋fileNameが取得してきた画像名に
-		
-		String uploadDir =
-				System.getProperty("user.dir")  //プロジェクトの中にあるupladsフォルダの場所を作って、その住所をuplpadDirという変数に入れてる
+
+	// 登録された画像ファイルをuploadsフォルダに保存
+	private String saveImage(MultipartFile file) throws IOException {
+		String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename(); // 現在時刻＋fileNameが取得してきた画像名に
+
+		String uploadDir = System.getProperty("user.dir") // プロジェクトの中にあるupladsフォルダの場所を作って、その住所をuplpadDirという変数に入れてる
 				+ "\\uploads\\";
-			
-				System.out.println("保存先：" + uploadDir);
-		
-		File dir = new File(uploadDir); //upLoadsフォルダを表すオブジェクトを作る
-		if(!dir.exists()) { //uploadsフォルダがない場合
-			dir.mkdirs(); //フォルダ作る
-			
+
+		System.out.println("保存先：" + uploadDir);
+
+		File dir = new File(uploadDir); // upLoadsフォルダを表すオブジェクトを作る
+		if (!dir.exists()) { // uploadsフォルダがない場合
+			dir.mkdirs(); // フォルダ作る
+
 		}
-		File saveFile = new File(dir, fileName); //保存する画像のパスを作成
-		file.transferTo(saveFile); //フォルダへ保存
-		return fileName;  //fileName = saveImage(image)へ戻る
+		File saveFile = new File(dir, fileName); // 保存する画像のパスを作成
+		file.transferTo(saveFile); // フォルダへ保存
+		return fileName; // fileName = saveImage(image)へ戻る
 	}
-	
-	//食事内容の更新
+
+	// 食事内容の更新
 	@PostMapping("/api/meal/update/")
-	private Meal update(
-			@RequestParam(value ="image", required=false) MultipartFile image,
-			@ModelAttribute Meal meal) {
-		
-		Integer userId = 1;  //ログイン中のユーザーのｉｄ取得→１は仮置き
-		meal.setUserId(userId);  //mealオブジェクトのuserIdに値をセット
-		
+	private Meal updateupdate(@RequestParam(value = "image", required = false) MultipartFile image,
+			@ModelAttribute Meal meal, HttpSession session) {
+
+		User loginUser = (User) session.getAttribute("loginUser");
+		if (loginUser == null) {
+		    throw new RuntimeException("ログインしてください");
+		}
+		Integer userId = loginUser.getUserId();
+
+		meal.setUserId(userId);
+
 		try {
-			//DBに保存されている既存の食事データを取得
-			Meal dbMeal =
-					repository.findById(meal.getMealId())
-					.orElseThrow();
-					
-					dbMeal.setUserId(userId); //仮のログインユーザーIDをセット
-					//更新画面で入力された値で上書き
-					dbMeal.setRecipeTitle(meal.getRecipeTitle());
-					dbMeal.setRecordDate(meal.getRecordDate());
-					dbMeal.setMealType(meal.getMealType());
-					dbMeal.setUrl(meal.getUrl());
-					dbMeal.setRecipeMemo(meal.getRecipeMemo());
-					//新しい画像が選択されている場合のみ画像を更新
-					if(image != null && !image.isEmpty()) {
-						//upLoadsフォルダへ画像保存
-					String fileName = saveImage(image);
-						//ファイル名セット
-					dbMeal.setMealImage(fileName);
-					}
-					
-			repository.save(dbMeal);  //DB保存
+			// DBに保存されている既存の食事データを取得
+			Meal dbMeal = repository.findById(meal.getMealId()).orElseThrow();
+
+			dbMeal.setUserId(userId); // 仮のログインユーザーIDをセット
+			// 更新画面で入力された値で上書き
+			dbMeal.setRecipeTitle(meal.getRecipeTitle());
+			dbMeal.setRecordDate(meal.getRecordDate());
+			dbMeal.setMealType(meal.getMealType());
+			dbMeal.setUrl(meal.getUrl());
+			dbMeal.setRecipeMemo(meal.getRecipeMemo());
+			// 新しい画像が選択されている場合のみ画像を更新
+			if (image != null && !image.isEmpty()) {
+				// upLoadsフォルダへ画像保存
+				String fileName = saveImage(image);
+				// ファイル名セット
+				dbMeal.setMealImage(fileName);
+			}
+
+			repository.save(dbMeal); // DB保存
 			return dbMeal;
-		
+
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	//食事内容の一覧取得+記録日が新しい順古い順に切り替え
-	@GetMapping("/api/meal/") 
-	List<Meal> get(
-					@RequestParam(defaultValue = "0") Integer page, //一覧表示のページデータを取得(使わないかも）
-					@RequestParam(defaultValue="desc")String sort
-				){
-		Integer userId = 1;  //ログイン中のユーザーのｉｄ取得→１は仮置き
+
+	// 食事内容の一覧取得+記録日が新しい順古い順に切り替え
+	@GetMapping("/api/meal/")
+	List<Meal> get(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "desc") String sort,
+			HttpSession session) {
+		User loginUser = (User) session.getAttribute("loginUser");
+		if (loginUser == null) {
+		    throw new RuntimeException("ログインしてください");
+		}
+		Integer userId = loginUser.getUserId();
 		Sort sortOrder;
-		if(sort.equals("asc")) {
+		if (sort.equals("asc")) {
 			sortOrder = Sort.by("recordDate").ascending();
-		}else {
+		} else {
 			sortOrder = Sort.by("recordDate").descending();
 		}
-		
-		//ページング情報
-		Pageable pageable=
-				PageRequest.of(
-						page, //何ページ目を取得するか
-						30,//一ページ当たり３０件
-						sortOrder //並び順
-				);
-		return repository.findByUserId(userId, pageable) //userIdが一致する食事記録の取得、ページングの条件
-				 .getContent(); //Page<mear>をList<meal>に変換
+
+		// ページング情報
+		Pageable pageable = PageRequest.of(page, // 何ページ目を取得するか
+				30, // 一ページ当たり３０件
+				sortOrder // 並び順
+		);
+		return repository.findByUserId(userId, pageable) // userIdが一致する食事記録の取得、ページングの条件
+				.getContent(); // Page<mear>をList<meal>に変換
 	}
-	
-	
-	//食事タイプ＋並び替え
-	@GetMapping("/api/meal/type/") 
-	List<Meal>getByType(
-					@RequestParam String mealType,
-					@RequestParam(defaultValue = "0") Integer page, //一覧表示のページデータを取得(使わないかも）
-					@RequestParam(defaultValue="desc")String sort
-				){
-		Integer userId = 1;  //ログイン中のユーザーのｉｄ取得→１は仮置き
+
+	// 食事タイプ＋並び替え
+	@GetMapping("/api/meal/type/")
+	List<Meal> getByType(@RequestParam String mealType, @RequestParam(defaultValue = "0") Integer page,
+			@RequestParam(defaultValue = "desc") String sort, HttpSession session) {
+		User loginUser = (User) session.getAttribute("loginUser");
+		if (loginUser == null) {
+		    throw new RuntimeException("ログインしてください");
+		}
+		Integer userId = loginUser.getUserId();
 		Sort sortOrder;
-		if(sort.equals("asc")) {
+		if (sort.equals("asc")) {
 			sortOrder = Sort.by("recordDate").ascending();
-		}else {
+		} else {
 			sortOrder = Sort.by("recordDate").descending();
 		}
-		
-		//ページング情報
-		Pageable pageable=
-				PageRequest.of(
-						page, //何ページ目を取得するか
-						30,//一ページ当たり３０件
-						sortOrder //並び順
-				);
-		return repository.findByUserIdAndMealType(
-							userId,
-							mealType,
-							pageable)
-						 .getContent(); //Page<mear>をList<meal>に変換
+
+		// ページング情報
+		Pageable pageable = PageRequest.of(page, // 何ページ目を取得するか
+				30, // 一ページ当たり３０件
+				sortOrder // 並び順
+		);
+		return repository.findByUserIdAndMealType(userId, mealType, pageable).getContent(); // Page<mear>をList<meal>に変換
 	}
-	
 
 }
--- A2/src/main/java/com/example/demo/repository/MealRepository.java
+++ A2/src/main/java/com/example/demo/repository/MealRepository.java
@@ -8,14 +8,9 @@ import com.example.demo.entity.Meal;
 
 public interface MealRepository extends JpaRepository<Meal, Integer> {
 
-	//ユーザーの食事情報の取得（一覧)
-	Page<Meal>findByUserId(
-			Integer userId,
-			Pageable pageable);  //ページング条件
-	
-	//ユーザー(一覧)+朝昼晩
-	Page<Meal>findByUserIdAndMealType(
-			Integer userId, 
-			String mealType,
-			Pageable pageable);
+	// ユーザーの食事情報の取得（一覧)
+	Page<Meal> findByUserId(Integer userId, Pageable pageable); // ページング条件
+
+	// ユーザー(一覧)+朝昼晩
+	Page<Meal> findByUserIdAndMealType(Integer userId, String mealType, Pageable pageable);
 }
