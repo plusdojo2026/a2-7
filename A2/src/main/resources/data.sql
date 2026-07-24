a3e89b426b674731fe534c328568402e0eff4c98
plusdojo2026-kaneko <kaneko-mana-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-24 16:04:24 +0900

    画像追加

--- A2/src/main/resources/data.sql
+++ A2/src/main/resources/data.sql
@@ -50,11 +50,11 @@ VALUES
 INSERT INTO Meals
 (record_date, meal_type, meal_image, url, recipe_memo, recipe_title, user_id)
 VALUES
-('2026-07-14', '朝食', NULL, 'https://example.com/recipe1', 'トーストはこんがり焼く', 'ハムエッグトースト', 1),
-('2026-07-14', '昼食', NULL, 'https://example.com/recipe2', '野菜を多めに入れる', 'チキンサラダ', 1),
-('2026-07-14', '夕食', NULL, 'https://example.com/recipe3', '弱火で20分煮込む', 'ビーフシチュー', 1),
-('2026-07-15', '朝食', NULL, 'https://example.com/recipe4', 'バナナは最後に盛り付ける', 'フルーツヨーグルト', 2),
-('2026-07-15', '昼食', NULL, 'https://example.com/recipe5', 'パスタはアルデンテに', 'ナポリタン', 2);
+('2026-07-14', '朝', ハムエッグトースト.png, 'https://example.com/recipe1', 'トーストはこんがり焼く', 'ハムエッグトースト', 1),
+('2026-07-14', '昼', チキンサラダ.png, 'https://example.com/recipe2', '野菜を多めに入れる', 'チキンサラダ', 1),
+('2026-07-14', '夕', ビーフシチュー.png, 'https://example.com/recipe3', '弱火で20分煮込む', 'ビーフシチュー', 1),
+('2026-07-15', '朝', NULL, 'https://example.com/recipe4', 'バナナは最後に盛り付ける', 'フルーツヨーグルト', 2),
+('2026-07-15', '昼', NULL, 'https://example.com/recipe5', 'パスタはアルデンテに', 'ナポリタン', 2);
 
 -- 食材マスター
 INSERT INTO Food_Master
