83cd08215904dc3d0bb6bbd3754fdae6c450a726
plusdojo2026-yamaguchi <yamaguchi-yu-plusdojo2026@seplus2016.onmicrosoft.com>
2026-07-27 15:46:23 +0900

    Update Stock.jsx

--- react/src/components/Stock.jsx
+++ react/src/components/Stock.jsx
@@ -476,17 +476,11 @@ const Stock = () => {
                                                 <div className="stock-list-image food">
                                                     <img
                                                         src={
-                                                            foodCategoryImages[
-                                                            food.category
-                                                            ] ??
-                                                            foodCategoryImages[
-                                                            "その他"
-                                                            ]
+                                                            food.foodImg
+                                                                ? `/img/${food.foodImg}`
+                                                                : foodCategoryImages[food.category]
                                                         }
-                                                        alt={
-                                                            food.foodStockName
-                                                        }
-                                                        className="stock-icon"
+                                                        alt={food.foodStockName}
                                                     />
                                                 </div>
 
